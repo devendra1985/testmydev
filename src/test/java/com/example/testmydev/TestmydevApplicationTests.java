d9da7f4186a8bb3fead7d1f474b9c822342de588
devendra1985 <30473698+devendra1985@users.noreply.github.com>
2026-02-15 15:25:30 +0530

    Add basic Spring Boot test class

---
+++ src/test/java/com/example/testmydev/TestmydevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmydevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
