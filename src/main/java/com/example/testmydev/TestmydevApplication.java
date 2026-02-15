8cdeb4a02a9b608853893f802d8d34cf3bf5c93a
devendra1985 <30473698+devendra1985@users.noreply.github.com>
2026-02-15 15:25:01 +0530

    Add main Spring Boot application class

---
+++ src/main/java/com/example/testmydev/TestmydevApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmydevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmydevApplication.class, args);
+	}
+
+}
