42f518e2f0d19aa17b911c1d85fad6dfd8ce3e7c
taledevendra <dtale@intuit.com>
2026-04-17 15:02:25 +0530

    splunk logs

--- src/main/java/com/example/testmydev/TestmydevApplication.java
+++ src/main/java/com/example/testmydev/TestmydevApplication.java
@@ -2,12 +2,24 @@ package com.example.testmydev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class TestmydevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestmydevApplication.class, args);
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(false);
+		filter.setIncludeHeaders(false);
+		filter.setIncludeClientInfo(true);
+		return filter;
 	}
 
 }
